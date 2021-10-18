16796dd4c2104e2dcfff1e509d837f7f084a8001
Brian Opao <brianopao@gmail.com>
2021-10-17 18:07:24 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+
+import Home from './components/Home';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import Error from './components/Error';
+
+
+function App(props){
+  return (
+    <div>
+      <Navbar />
+      <Switch>
+        <Route path='/' component={Home} exact/>
+        <Route path='/about' component={About} exact/>
+        <Route component={Error}/>
+        </Switch>
+    </div>
+  );
+}
+
+export default App;
