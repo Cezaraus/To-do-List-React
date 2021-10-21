05276ae5974fdcb34f055ac3754511cbf91c051c
Brian Opao <brianopao@gmail.com>
2021-10-21 10:13:06 -0700

    Updated About page with some information

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,11 @@ function About(){
     return(
         <div className="todoapp stack-large">
             <h1> The About Page </h1>
+            <p>
+                Hi there! Thank you for checking out my first react app project.
+                You can checkout the repository on my Github repo 
+                <a href="https://github.com/Cezaraus/To-do-List-React"> here.</a>
+            </p>
         </div>
     );
 }
